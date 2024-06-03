2d197abac69338c4625525d151ad909bd478b797
ggumi030 <tndus50843@gmail.com>
2024-06-03 19:43:21 +0900

    [init] 프로젝트 초기 파일 생성

---
+++ src/main/java/com/sparta/newsfeedteamproject/NewsfeedTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/newsfeedteamproject/NewsfeedTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
