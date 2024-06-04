07db2468c04519a7be71c7cad2fb7b530a428331
hongsy521 <seoyeong0521@naver.com>
2024-06-05 00:48:32 +0900

    [feat] User Entity 생성

--- src/main/java/com/sparta/newsfeedteamproject/controller/UserController.java
+++ src/main/java/com/sparta/newsfeedteamproject/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.sparta.newsfeedteamproject.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
 }
--- src/main/java/com/sparta/newsfeedteamproject/entity/User.java
+++ src/main/java/com/sparta/newsfeedteamproject/entity/User.java
@@ -1,4 +1,38 @@
 package com.sparta.newsfeedteamproject.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@RequiredArgsConstructor
+public class User extends Timestamp{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String login_id;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column
+    private String user_info;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Column
+    private String refresh_token;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime status_mod_time;
 }
