8c216c628b86ece142ec0509ef5212ffe888ea8c
hongsy521 <seoyeong0521@naver.com>
2024-06-05 01:39:33 +0900

    [feat] SignupReqDto 생성

--- src/main/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDto.java
+++ src/main/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDto.java
@@ -1,4 +1,23 @@
 package com.sparta.newsfeedteamproject.dto.user;
 
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+
+@Getter
 public class SignupReqDto {
+
+    @NotBlank
+    @Size(min = 10, max = 20)
+    @Pattern(regexp = "^[0-9a-zA-Z]+$")
+    private String login_id;
+    @NotBlank
+    @Size(min = 10)
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$")
+    private String password;
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Email
+    private String email;
+    private String user_info;
 }
