95a3bc4651de582f6da9c5a07f3fcbe2a9881c09
ggumi030 <tndus50843@gmail.com>
2024-06-05 22:29:26 +0900

    [feat] MethodArgumentNotValidExceptionHandler 구현

--- src/main/java/com/sparta/newsfeedteamproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/newsfeedteamproject/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.sparta.newsfeedteamproject.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +15,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> IllegalArgumentExceptionHandler(IllegalArgumentException e){
         return new ResponseEntity<>("Exception caught : " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e){
+        String errmsgs = e.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", "));
+        return new ResponseEntity<>(errmsgs, HttpStatus.BAD_REQUEST);
     }
 }
