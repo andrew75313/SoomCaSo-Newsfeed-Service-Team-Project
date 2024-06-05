cae6177f9a5764d20fcdecbcb135fa9524134924
ggumi030 <tndus50843@gmail.com>
2024-06-05 19:25:26 +0900

    [feat] 인가 필터 기능 구현
    - 토큰 검증
    - 인증한 후, 인증 객체 생성
    
    [feat] 인증 필터 기능 구현
    
    - 탈퇴한 회원 예외처리

--- src/main/java/com/sparta/newsfeedteamproject/security/AuthenticationFilter.java
+++ src/main/java/com/sparta/newsfeedteamproject/security/AuthenticationFilter.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.newsfeedteamproject.dto.user.UserAuthReqDto;
 import com.sparta.newsfeedteamproject.exception.FilterExceptionHandler;
 import com.sparta.newsfeedteamproject.jwt.JwtProvider;
+import com.sparta.newsfeedteamproject.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,26 +21,43 @@ import java.io.IOException;
 @Slf4j(topic = "로그인 및 JWT 생성")
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final JwtProvider jwtProvider;
+    private final UserDetailsImpl userDetails;
 
-    public AuthenticationFilter(JwtProvider jwtProvider) {
+    public AuthenticationFilter(JwtProvider jwtProvider, UserDetailsImpl userDetails) {
         this.jwtProvider = jwtProvider;
+        this.userDetails = userDetails;
         setFilterProcessesUrl("/users/login");
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         log.info("로그인 시도");
+
         try {
             UserAuthReqDto requestDto = new ObjectMapper().readValue(request.getInputStream(), UserAuthReqDto.class);
-
-            return getAuthenticationManager().authenticate(
+            //로그인 시도
+            Authentication auth = getAuthenticationManager().authenticate(
                     new UsernamePasswordAuthenticationToken(
-                            requestDto.getLoginId(),
+                            requestDto.getUsername(),
                             requestDto.getPassword(),
                             null
                     )
             );
-        } catch (IOException e) {
+//            auth.getDetails() == ((UserDetailsImpl) auth.getPrincipal())
+
+            //유저 상태 확인
+            if("ACTIVATE".equal(userDetails.getUser().getStatus())){
+                log.error("탈퇴한 회원");
+                throw new AccountStatusException("탈퇴한 회원입니다.") {
+                    @Override
+                    public String getMessage() {
+                        return super.getMessage();
+                    }
+                };
+            }
+
+            return auth;
+        } catch (IOException | AccountStatusException e) {
             log.error(e.getMessage());
             FilterExceptionHandler.handleExceptionInFilter(response,e);
         }
@@ -47,15 +66,18 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        log.info("로그인 성공 및 JWT 생성");
-        String userId = ((UserDetailsImpl) authResult.getPrincipal()).getLoginId();
+        log.info("로그인 성공");
+
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
         Status status = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getStatus();
 
-        String accesstoken = jwtProvider.createAccessToken(userId, status);
-        String refreshtoken = jwtProvider.createRefreshToken(userId, status);
+        String accesstoken = jwtProvider.createAccessToken(username, status);
+        String refreshtoken = jwtProvider.createRefreshToken(username, status);
 
-        response.addHeader(JwtProvider.ACCESS_TOKEN, accesstoken);
-        response.addHeader(JwtProvider.REFRESH_TOKEN, refreshtoken);
+        log.info("JWT 생성");
+
+        response.addHeader(JwtProvider.ACCESS_TOKEN_HEADER, accesstoken);
+        response.addHeader(JwtProvider.REFRESH_TOKEN_HEADER, refreshtoken);
     }
 
     @Override
