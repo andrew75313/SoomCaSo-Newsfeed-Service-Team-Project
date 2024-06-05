752ddbd3d77e7949a8943c891f8b9a5a9d137cbc
berithx <seere7777@gmail.com>
2024-06-05 12:23:11 +0900

    [feat] 로그아웃
    - 로그아웃 API  구현
    - User 객체 내 Refresh 토큰 제거 메서드 구현

--- src/main/java/com/sparta/newsfeedteamproject/entity/User.java
+++ src/main/java/com/sparta/newsfeedteamproject/entity/User.java
@@ -1,4 +1,17 @@
 package com.sparta.newsfeedteamproject.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "refresh_token")
+    String refreshToken;
+
+    public void deleteRefreshToken() {
+         this.refreshToken = "";
+    }
 }
