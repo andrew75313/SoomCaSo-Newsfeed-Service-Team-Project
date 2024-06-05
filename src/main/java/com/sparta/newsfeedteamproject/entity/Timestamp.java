55eb9f91feb3d90123617d512943992ccac5d253
hongsy521 <seoyeong0521@naver.com>
2024-06-06 01:04:24 +0900

    [update] 예외 추가

--- src/main/java/com/sparta/newsfeedteamproject/entity/Timestamp.java
+++ src/main/java/com/sparta/newsfeedteamproject/entity/Timestamp.java
@@ -2,6 +2,7 @@ package com.sparta.newsfeedteamproject.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Timestamp {
