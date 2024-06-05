5759881018efaee8e4d3308909cc373787814861
ggumi030 <tndus50843@gmail.com>
2024-06-05 22:11:54 +0900

    [feat] IllegalArgumentExceptionHandler 구현

--- src/main/java/com/sparta/newsfeedteamproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/newsfeedteamproject/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.sparta.newsfeedteamproject.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> IllegalArgumentExceptionHandler(IllegalArgumentException e){
+        return new ResponseEntity<>("Exception caught : " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
