a490abe124993e615ec5cf79946c74b6a85b86ab
seungsuuu <tmdtn013@naver.com>
2024-06-05 15:28:14 +0900

    [feat] accessToken 생성 기능 추가
    - 토큰 생성에 필요한 데이터 선언
    - createAccessToken 메서드 구현
    - jwt.secret.key 값 설정

--- src/main/java/com/sparta/newsfeedteamproject/jwt/JwtProvider.java
+++ src/main/java/com/sparta/newsfeedteamproject/jwt/JwtProvider.java
@@ -1,4 +1,50 @@
 package com.sparta.newsfeedteamproject.jwt;
 
+import com.sparta.newsfeedteamproject.entity.Status;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JwtProvider")
+@Component
 public class JwtProvider {
+
+    public static final String ACCESS_TOKEN = "accessToken";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String AUTHORIZATION_KEY = "status";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private final long ACCESS_TOKEN_TIME = 60 * 60 * 1000L;
+    private final long REFRESH_TOKEN_TIME = 24 * 60 * 60 * 1000L;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.ES256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createAccessToken(String username, Status status) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username)
+                        .setExpiration(new Date(date.getTime() + ACCESS_TOKEN_TIME))
+                        .claim(AUTHORIZATION_KEY, status)
+                        .setIssuedAt(date)
+                        .signWith(key, signatureAlgorithm)
+                        .compact();
+    }
 }
