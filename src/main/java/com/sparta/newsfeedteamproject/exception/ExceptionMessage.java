d3d1f47ce1ff691cd9e6517e7246d311a5559350
ggumi030 <tndus50843@gmail.com>
2024-06-07 16:13:02 +0900

    [feat] ExceptionMessage
    -AuthorizationFilter 예외 메세지 Enum에 추가

--- src/main/java/com/sparta/newsfeedteamproject/exception/ExceptionMessage.java
+++ src/main/java/com/sparta/newsfeedteamproject/exception/ExceptionMessage.java
@@ -2,7 +2,8 @@ package com.sparta.newsfeedteamproject.exception;
 
 public enum ExceptionMessage {
 
-    DIFFERENT_WRITER("해당 게시물은 작성자만 수정/삭제 할 수 있습니다!");
+    DIFFERENT_WRITER("해당 게시물은 작성자만 수정/삭제 할 수 있습니다!"),
+    DEATIVATE_USER("탈퇴한 회원입니다.");
 
     private String message;
 
