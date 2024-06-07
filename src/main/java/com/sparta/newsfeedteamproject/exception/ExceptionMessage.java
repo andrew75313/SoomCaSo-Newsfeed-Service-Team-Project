662846956ebe511c1a8c8f8300319a48883863bf
ggumi030 <tndus50843@gmail.com>
2024-06-07 16:03:07 +0900

    [add] ExceptionMessage
    -예외 메세지 관리 Enum 클래스 생성

---
+++ src/main/java/com/sparta/newsfeedteamproject/exception/ExceptionMessage.java
@@ -0,0 +1,16 @@
+package com.sparta.newsfeedteamproject.exception;
+
+public enum ExceptionMessage {
+
+    DIFFERENT_WRITER("해당 게시물은 작성자만 수정/삭제 할 수 있습니다!");
+
+    private String message;
+
+    private ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getExceptionMessage(){
+        return this.message;
+    }
+}
