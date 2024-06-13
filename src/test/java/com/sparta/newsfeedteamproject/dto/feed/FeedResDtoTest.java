90767afc903ff60fb3ddc76fbb83ef2f343404ce
ggumi030 <tndus50843@gmail.com>
2024-06-13 17:56:31 +0900

    [test] FeedResDto test

---
+++ src/test/java/com/sparta/newsfeedteamproject/dto/feed/FeedResDtoTest.java
@@ -0,0 +1,30 @@
+package com.sparta.newsfeedteamproject.dto.feed;
+
+import com.sparta.newsfeedteamproject.dto.comment.CommentResDto;
+import com.sparta.newsfeedteamproject.entity.Feed;
+import com.sparta.newsfeedteamproject.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class FeedResDtoTest {
+    @Test
+    @DisplayName("FeedResDto test")
+    public void test(){
+        //given
+        List<CommentResDto> comments = new ArrayList<>();
+        FeedResDto feedResDto = Mockito.mock(FeedResDto.class);
+
+        //when
+        feedResDto.setComments(comments);
+
+        //then
+        assertEquals(comments, feedResDto.getComments());
+    }
+}
