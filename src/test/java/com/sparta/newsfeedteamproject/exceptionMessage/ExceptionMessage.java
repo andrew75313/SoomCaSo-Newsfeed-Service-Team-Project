2336eb66068b0eddd3c87c297c616d5ae064e75a
ggumi030 <tndus50843@gmail.com>
2024-06-17 12:52:22 +0900

    [refactor] ExceptionMessage.java 파일 삭제

--- src/test/java/com/sparta/newsfeedteamproject/exceptionMessage/ExceptionMessage.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.newsfeedteamproject.exceptionMessage;
-
-public class ExceptionMessage {
-
-    public static String nullField(String fieldName) {
-        return String.format("%s는 null일 수 없습니다.", fieldName);
-    }
-}
